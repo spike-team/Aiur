fc616e4aac5431b593c91307e53ba866a1396678
Jinsab <kimjinou116@naver.com>
2019-10-30 11:34:17 +0900

    [Update] Main Import Form

--- src/main/main/main.java
+++ src/main/main/main.java
@@ -1,6 +1,8 @@
 import com.google.common.collect.*;
+
 import java.util.*;
 import java.io.*;
+
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -13,24 +15,31 @@ public class main {
 
         Multimap<Integer, Table> time_table = ArrayListMultimap.create();
 
-        int[] dy = {0, 0, 0, 0, 0};
-        int[] arr = {7, 7, 7, 7, 6};
+        int[] arr;
+        int[][] reservation_Int;
+        String[][] reservation_String;
 
-        Integer full_time = 28, day = 0, period = 0;
+        Integer full_time, day = 0, period = 0, group;
         ExcelParser excelParser = new ExcelParser();
 
         excelParser.Import(time_table);
 
+        full_time = excelParser.getFull_time();
+        group = excelParser.getGroup();
+        arr = excelParser.getArr();
+        reservation_Int = excelParser.getReservation_time();
+        reservation_String = excelParser.getReservation_block();
+
         student_table set_table = new student_table();
 
-        Integer grade = 0, group = 2;
+        Integer grade = 0;
 
         String[][][] student = new String[10][5][group];
         String[][][] teacher = new String[10][5][group];
 
         while (grade < group) {
             if
-            (!set_table.createTimeTable(time_table, full_time, period, day, student, teacher, arr, grade)) {
+            (!set_table.createTimeTable(time_table, reservation_Int, reservation_String, full_time, period, day, student, teacher, arr, grade)) {
                 grade++;
             }
         }
@@ -70,7 +79,7 @@ class student_table {
     private Integer cnt = 0;
     private Integer knt = 0;
 
-    public boolean createTimeTable(Multimap<Integer, Table> time_table, Integer full_time, Integer periot, Integer dat, String[][][] student, String[][][] teacher, int[] arr, Integer grade) {
+    public boolean createTimeTable(Multimap<Integer, Table> time_table, int[][] reservation_time, String[][] reservation_block,Integer full_time, Integer periot, Integer dat, String[][][] student, String[][][] teacher, int[] arr, Integer grade) {
         Multimap<Integer, Table> time_table_copy = ArrayListMultimap.create();
 
         Iterator<Integer> tableInteger = time_table.keys().iterator();
@@ -86,6 +95,23 @@ class student_table {
         String[][] stu = new String[10][5];
         String[][] tea = new String[10][5];
 
+        for (int i = 0; i < reservation_block.length; i++) {
+            stu[reservation_time[i][1]][reservation_time[i][0]] = reservation_block[i][0];
+            tea[reservation_time[i][1]][reservation_time[i][0]] = reservation_block[i][1];
+        }
+        /*
+        stu[0][0] = "창체";
+        tea[0][0] = "안현수";
+
+        stu[4][3] = "창체";
+        tea[4][3] = "안현수";
+
+        stu[5][3] = "창체";
+        tea[5][3] = "임채홍";
+
+        stu[6][3] = "창체";
+        tea[6][3] = "임채홍";
+        */
         Integer day = dat;
         Integer period = periot;
 
@@ -98,8 +124,9 @@ class student_table {
                 break;
             }
 
-            if (stu[period][day] != null)
+            if (stu[period][day] != null) {
                 period++;
+            }
 
             knt++;
             pc++;
@@ -159,18 +186,22 @@ class student_table {
             if (!overlap) {
                 if (!(table.time > 1 && period + table.time > 4 && period <= 3)) {
                     if (!(period + table.time > arr[day])) {
-                        for (int m = 0; m < table.time; m++) {
-                            String str1 = table.cource.firstElement();
-                            String str2 = table.cource.lastElement();
+                        if (stu[period][day] == null && tea[period][day] == null) {
+                            for (int m = 0; m < table.time; m++) {
+                                String str1 = table.cource.firstElement();
+                                String str2 = table.cource.lastElement();
 
-                            stu[period][day] = str1;
-                            tea[period][day] = str2;
 
-                            period++;
+                                stu[period][day] = str1;
+                                tea[period][day] = str2;
+
+                                period++;
+
+                            }
+
+                            time_table_copy.remove(key, table);
+                            pc = 0;
                         }
-
-                        time_table_copy.remove(key, table);
-                        pc = 0;
                     }
                 }
             }
@@ -207,6 +238,12 @@ class student_table {
 }
 
 class ExcelParser {
+    private int full_time = 0;
+    private int group = 0;
+    private int[] arr = new int[0];
+    private int[][] reservation_time = new int[0][0];
+    private String[][] reservation_block = new String[0][0];
+
     public static final String projectPath = System.getProperty("user.dir");
 
     public void Import(Multimap<Integer, Table> time_table) {
@@ -244,6 +281,41 @@ class ExcelParser {
 
                         time_table.put(j, new Table(key, value));
                     }
+
+                    XSSFCell cell_four = sheet.getRow(0).getCell(4); // 블록갯수 full_time
+                    XSSFCell cell_five = sheet.getRow(1).getCell(4); // 반 group
+                    XSSFCell cell_six = sheet.getRow(0).getCell(7); // for 반복횟수
+
+                    full_time = (int) cell_four.getNumericCellValue();
+                    group = (int) cell_five.getNumericCellValue();
+
+                    int day_number = (int) cell_six.getNumericCellValue();
+                    arr = new int[day_number];
+
+                    for (int i = 0; i < day_number; i++) {
+                        XSSFCell cell_seven = sheet.getRow(i).getCell(6);
+                        int arrayNumber = (int) cell_seven.getNumericCellValue();
+
+                        arr[i] = arrayNumber;
+                    }
+
+                    XSSFCell cell_eight = sheet.getRow(0).getCell(12);
+                    int reservation_number = (int) cell_eight.getNumericCellValue();
+
+                    reservation_time = new int[reservation_number][reservation_number];
+                    reservation_block = new String[reservation_number][reservation_number];
+
+                    for (int k = 0; k < reservation_number; k++) {
+                        XSSFCell cell_class = sheet.getRow(k).getCell(8);
+                        XSSFCell cell_day = sheet.getRow(k).getCell(9);
+                        reservation_time[k][0] = (int) cell_class.getNumericCellValue() - 1;
+                        reservation_time[k][1] = (int) cell_day.getNumericCellValue() - 1;
+
+                        XSSFCell cell_subject = sheet.getRow(k).getCell(10);
+                        XSSFCell cell_teacher = sheet.getRow(k).getCell(11);
+                        reservation_block[k][0] = cell_subject.getStringCellValue();
+                        reservation_block[k][1] = cell_teacher.getStringCellValue();
+                    }
                 }
             }
         } catch (Exception e) {
@@ -251,7 +323,7 @@ class ExcelParser {
         }
     }
 
-    public void Export(String [][][] dataArray, int group) {
+    public void Export(String[][][] dataArray, int group) {
         FileOutputStream fos = null;
 
         try {
@@ -268,7 +340,7 @@ class ExcelParser {
 
         for (int i = 0; i < group; i++) {
             for (int j = 0; j < dataArray.length; j++) {
-                row = sheet.createRow(j + (i*10));
+                row = sheet.createRow(j + (i * 10));
 
                 for (int k = 0; k < dataArray[j].length; k++) {
                     cell = row.createCell(k);
@@ -288,5 +360,25 @@ class ExcelParser {
         }
 
         System.out.println("정상 작동 완료.");
+    }
+
+    public int getFull_time() {
+        return full_time;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public int[] getArr() {
+        return arr;
+    }
+
+    public int[][] getReservation_time() {
+        return reservation_time;
+    }
+
+    public String[][] getReservation_block() {
+        return reservation_block;
     }
 }
